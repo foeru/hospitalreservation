88430fa2b9d2e7be5446a5555672cbf77b3cfcdc
foeru <yerilotti@gmail.com>
2025-06-24 12:01:24 +0900

    commit

---
+++ src/main/java/com/foeru/hospitalreservation/dto/ResponseMessage.java
@@ -0,0 +1,28 @@
+package com.foeru.hospitalreservation.dto;
+
+public class ResponseMessage {
+
+    private String message;
+    private Object data;
+
+    public ResponseMessage(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
